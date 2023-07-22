e5dfe2b10afc05375e53401839fec498603616bb
unknown <manoj.koranga144@gmail.com>
2023-07-22 18:03:39 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import Home from "./Components/Home/index.js";
+import { useState, createContext } from "react";
+import "./App.css";
 
+const ThemeContext = createContext("light");
 function App() {
+  const [theme, setTheme] = useState("light");
+  const toggleTheme = () => {
+    setTheme((curr) => (curr === "light" ? "dark" : "light"));
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      <div className="app" id={theme}>
+        <Home />
+      </div>
+    </ThemeContext.Provider>
   );
 }
 
 export default App;
+export { ThemeContext };
+
---
+++ src/Components/Home/index.js
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from "react";
+import { Frown } from "react-feather";
+import ModalDiv from "../Modal/index.js";
+import Nav from "../Navbar/index.js";
+import SingleNote from "../SingleNotes/index.js";
+import "./Styles.css";
+import Modal from "../Modal/modal.js";
+const Home = () => {
+  const [data, setData] = useState([]);
+  const [showModal, setShowModal] = useState(false);
+
+  useEffect(() => {
+    setData(JSON.parse(localStorage.getItem("myNotes")) || []);
+  }, []);
+
+  const refresher = () => {
+    setData(JSON.parse(localStorage.getItem("myNotes")) || []);
+  };
+
+  return (
+    <>
+      <Nav
+        showModal={showModal}
+        setShowModal={setShowModal}
+        data={data}
+        setData={setData}
+        refresher={refresher}
+      />
+  
+      {showModal && (
+        <ModalDiv
+          showModal={showModal}
+          setShowModal={setShowModal}
+          refresher={refresher}
+        />
+      )}
+
+      {/* notes */}
+      <div className="row justify-content-between mx-0 p-5">
+        {!data.length ? (
+          <h1 className="text-center display-1 fw-light text-seconday my-5">
+            <Frown size={100} /> No Notes. Create new one.
+          </h1>
+        ) : (
+          data.map((item, i) => (
+            <SingleNote key={item.id} item={item} refresher={refresher} />
+          ))
+        )}
+      </div>
+    </>
+  );
+};
+export default Home;
